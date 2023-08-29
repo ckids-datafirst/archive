167578d0eef6c95db401fd0ea3f70116c37de8bd
Maximiliano Osorio <maxiosorio@gmail.com>
2023-08-29 19:23:52 -0400

    fix: safely issue

--- datafest_archive/builder/templating.py
+++ datafest_archive/builder/templating.py
@@ -2,4 +2,6 @@
 
 from jinja2 import Environment, PackageLoader
 
-jinja_environment = Environment(loader=PackageLoader("datafest_archive", "templates"))
+jinja_environment = Environment(
+    loader=PackageLoader("datafest_archive", "templates"), autoescape=True
+)
